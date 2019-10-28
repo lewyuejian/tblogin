73dbb2955aaad1b2aa2ed47e421b1abad726466f
lewyuejian <lewyuejian@163.com>
2019-10-28 11:51:48 +0800

    工具模块视图

--- users/viewstools.py
+++ users/viewstools.py
@@ -5,6 +5,11 @@ from django.http import JsonResponse
 
 
 def bsjtool(request):
+    """
+    校验29协议数据视图方法
+    :param request:
+    :return:
+    """
     ctx={}
     if request.POST:
         data_o=request.POST['bsjpact']
@@ -12,12 +17,28 @@ def bsjtool(request):
         ctx['rlt']=check.check_bsj(data_o)
     return render(request, "means/bsjtool.html", ctx)
 
-
-
-def check_bsj_data(request):
-    ctx = {}
+def gbtool(request):
+    """
+    校验部标协议数据视图方法
+    :param request:
+    :return:
+    """
+    ctx={}
     if request.POST:
-        data_o = request.POST['bsjpact']
-        check = ProtocolCheck()
-        ctx['rlt'] = check.check_bsj(data_o)
-    return  render(request,"means/datacheck.html",ctx)
+        data_o=request.POST['gbpact']
+        check=ProtocolCheck()
+        ctx['rlt']=check.check_bsj(data_o)
+    return render(request, "means/bsjtool.html", ctx)
+
+def v3tool(request):
+    """
+    校验V3协议数据视图方法
+    :param request:
+    :return:
+    """
+    ctx={}
+    if request.POST:
+        data_o=request.POST['v3pact']
+        check=ProtocolCheck()
+        ctx['rlt']=check.check_bsj(data_o)
+    return render(request, "means/bsjtool.html", ctx)
